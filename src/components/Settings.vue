b94981fcc73f7f526d9b91fc45957681147fd118
Marco Schmalz <marco@schess.ch>
2018-05-31 22:52:59 +0200

    Indentation

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,21 +1,23 @@
 <template>
-<div class="section">
-  <h1 class="title">Einstellungen</h1>
-  <div class="field">
-    <label class="label">Speicher-ID</label>
-    <div class="control">
-      <input class="input" type="text" placeholder="" v-model="storageId">
-   </div>
-   <p class="help">Um auf deine existierende Tschuttiheftlibuchhaltung zugreifen zu können, hier einfach noch einmal dieselbe Speicher-ID eingeben.</p>
-  </div>
-  <div class="field is-grouped">
-  <div class="control">
-    <button class="button is-primary" @click="save">Speichern</button>
-  </div>
-  <div class="control">
-    <button class="button is-text" @click="cancel">Abrechen</button>
-  </div>
-</div>
+  <div class="section">
+    <h1 class="title">Einstellungen</h1>
+    <div class="field">
+      <label class="label">Speicher-ID</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="" v-model="storageId">
+    </div>
+    <p class="help">Um auf deine existierende Tschuttiheftlibuchhaltung zugreifen zu können, hier einfach noch einmal dieselbe Speicher-ID eingeben.</p>
+    </div>
+    <div class="field is-grouped">
+      <div class="control">
+        <button class="button is-primary" @click="save">Speichern</button>
+      </div>
+      <div class="control">
+        <button class="button is-text" @click="cancel">Abrechen</button>
+      </div>
+    </div>
+    <hr>
+    Code auf <a href="https://github.com/smlz/tschuttiheftlibuchhaltung">GitHub</a>
   </div>
 </template>
 

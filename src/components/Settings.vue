45209e1a99c6b2de998daedd860ae63801815926
Marco Schmalz <marco@schess.ch>
2018-06-02 18:08:46 +0200

    Confirm reusing storageId

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,43 +1,82 @@
 <template>
   <div class="section">
-    <h1 class="title">Einstellungen</h1>
+    <h1 class="title is-3">Einstellungen</h1>
     <div class="field">
       <label class="label">Speicher-ID</label>
       <div class="control">
-        <input class="input" type="text" placeholder="" v-model="storageId">
+        <input class="input" type="text" placeholder="" v-model="storageId" @keyup.enter="save">
     </div>
-    <p class="help">Um auf deine existierende Tschuttiheftlibuchhaltung zugreifen zu können, hier einfach noch einmal dieselbe Speicher-ID eingeben.</p>
+    <p class="help">Um auf deine existierende Tschuttiheftlibuchhaltung zugreifen zu können, hier einfach noch einmal dieselbe Speicher-ID eintragen.</p>
     </div>
-    <div class="field is-grouped">
+    <div class="field is-grouped is-right">
       <div class="control">
-        <button class="button is-primary" @click="save">Speichern</button>
+        <button class="button is-black" :class="{'is-loading': saving}" @click="save">Speichern</button>
       </div>
       <div class="control">
-        <button class="button is-text" @click="cancel">Abrechen</button>
+        <button class="button is-text" :disabled="saving" @click="cancel">Abrechen</button>
       </div>
     </div>
     <hr>
     Code auf <a href="https://github.com/smlz/tschuttiheftlibuchhaltung">GitHub</a>
+
+    <div class="modal" :class="{'is-active': showModal}">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Speicher-ID existiert bereits</p>
+        </header>
+        <section class="modal-card-body">
+          <p>Mit dieser Speicher-ID wurden bereits Tschuttiheftlibuchhaltungsdaten abgespeichert.</p><br>
+          <p>Bist du sicher, dass du sie verwenden möchtest?</p>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-black" @click="confirmed">Speicher-ID verwenden</button>
+          <button class="button is-text" @click="showModal = false">Abbrechen</button>
+        </footer>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import router from "@/router";
+import db from "@/db"
 
 export default {
   name: "Settings",
   data() {
     return {
-      storageId: localStorage.getItem("storageId") || ""
+      storageId: localStorage.getItem("storageId") || "",
+      saving: false,
+      showModal: false,
     };
   },
   methods: {
     save() {
-      localStorage.setItem("storageId", this.storageId);
-      router.push("/");
+      this.saving = true;
+      if (this.storageId !== localStorage.getItem("storageId")) {
+        db.ref(this.storageId).once('value', storage => {
+          if (storage.val() === null) {
+            localStorage.setItem("storageId", this.storageId)
+            Vue.set(this, 'saving', false)
+            router.push("/")
+          } else {
+            Vue.set(this, 'saving', false)
+            Vue.set(this, 'showModal', true)
+          }
+        })
+      } else {
+        router.push("/");
+      }
     },
     cancel() {
       this.storageId = localStorage.getItem("storageId") || "";
+      router.push("/");
+    },
+    confirmed() {
+      this.showModal = false
+      localStorage.setItem("storageId", this.storageId)
       router.push("/");
     }
   }

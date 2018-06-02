e90125b9c84f914fefa2953d3ec526ca5f3bf7d2
Marco Schmalz <marco@schess.ch>
2018-06-02 17:58:08 +0200

    Init db in seperate module

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -17,19 +17,8 @@
 
 <script>
 import Vue from 'vue'
-import Firebase from '@firebase/app'
-// eslint-disable-next-line no-unused-vars
-import FirebaseDatabase from '@firebase/database'
+import db from '@/db'
 import data from './data'
-
-const db = Firebase.initializeApp({
-  apiKey: "AIzaSyBKQW7pmD9eY89Im_A9zPjkgtyqIT9ogqQ",
-  authDomain: "tschuttiheftlibuchhaltung.firebaseapp.com",
-  databaseURL: "https://tschuttiheftlibuchhaltung.firebaseio.com",
-  projectId: "tschuttiheftlibuchhaltung",
-  storageBucket: "tschuttiheftlibuchhaltung.appspot.com",
-  messagingSenderId: "217301557004"
-}).database()
 
 var storage = undefined
 
---
+++ src/db.js
@@ -0,0 +1,12 @@
+import Firebase from '@firebase/app'
+// eslint-disable-next-line no-unused-vars
+import FirebaseDatabase from '@firebase/database'
+
+export default Firebase.initializeApp({
+    apiKey: "AIzaSyBKQW7pmD9eY89Im_A9zPjkgtyqIT9ogqQ",
+    authDomain: "tschuttiheftlibuchhaltung.firebaseapp.com",
+    databaseURL: "https://tschuttiheftlibuchhaltung.firebaseio.com",
+    projectId: "tschuttiheftlibuchhaltung",
+    storageBucket: "tschuttiheftlibuchhaltung.appspot.com",
+    messagingSenderId: "217301557004"
+  }).database()
